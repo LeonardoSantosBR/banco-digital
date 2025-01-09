1b71f07edd5c7cc79c69d9a26411b1e8c65db795
LeonardoSantosBR <leosantosmelo07@gmail.com>
2025-01-08 23:16:44 -0300

    feat: sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ClientsModule } from './modules/clients/clients.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 4000,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      models: [],
+    }),
+    ClientsModule,
+  ],
   controllers: [],
   providers: [],
 })
